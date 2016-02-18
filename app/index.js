83a65aa989a18d6300f8284028680caa8e491c46
Alex Layton <alex@layton.in>
2016-02-18 15:30:51 -0500

    Add jsdoc for license

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
-/* Copyright 2015 Open Ag Data Alliance
+/**
+ * @license Apache-2.0
+ * Copyright 2015 Open Ag Data Alliance
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
